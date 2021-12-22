59a043c13b43494cd38ca8a585762fea953955fb
Francesca Pompi <francesca.pompi93@gmail.com>
2021-12-22 15:55:14 +0100

    Classe Country

---
+++ src/main/java/org/generation/italy/Main.java
@@ -0,0 +1,15 @@
+package org.generation.italy;
+
+public class Main {
+	
+	private final static String DB_URL = "jdbc:mysql://localhost:3306/nations";
+	private final static String DB_USER = "root";
+	private final static String DB_PASSWORD = "g2jxepx7fp9hxg2";
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
